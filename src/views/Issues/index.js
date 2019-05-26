3b70479488a5286fcfe2a2da3579eecd99c824c0
tactnician <tactnician@gmail.com>
2019-05-26 14:42:55 -0400

    added Issue/index view

--- src/views/Issues/index.js
+++ src/views/Issues/index.js
@@ -4,3 +4,29 @@ Step1 Who Needs What and Why
 Step2 Google Location Search 
 Step3 
 */
+
+import React from 'react'
+
+export default function Issues(props) {
+
+    if (props.loading) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    } else {
+        return(
+            <div className="Issues">
+                { props.issues.map((issue) => {
+                    return(
+                        <div>
+                            {issue.title}
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+    
+}
